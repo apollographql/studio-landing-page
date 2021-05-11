ec5f3ce738dfa70d01e42dd76f35c3dab31e5c60
mayakoneval <mayakoneval@gmail.com>
2021-05-11 14:44:07 -0700

    setup logic for which page to show

--- src/App.tsx
+++ src/App.tsx
@@ -12,5 +12,14 @@ export default () => {
       )}`,
     );
   }
-  return <LocalUnconfigured endpoint={window.location.href} />;
+
+  const isProdGraph = !!window.graphRef;
+  const isConfigured = false; // TODO check with glasser what constitutes a configured graph
+  return isProdGraph && isConfigured ? (
+    <ProdConfigured />
+  ) : isProdGraph ? (
+    <ProdUnconfigured />
+  ) : (
+    <LocalUnconfigured endpoint={window.location.href} />
+  );
 };
