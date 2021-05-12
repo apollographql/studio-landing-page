0bda25d04414d8af4d78700ea795d1060e035e6b
mayakoneval <mayakoneval@gmail.com>
2021-05-12 08:45:07 -0700

    add local unconfigured page by copying pierre's html & moving each css class into emotion's css tag on a given component

---
+++ src/stories/LandingPages.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import LocalUnconfiguredComponent from '../LocalUnconfigured';
+
+export default {
+  title: 'Studio Landing Page/Landing Pages',
+} as Meta;
+
+export const LocalUnconfigured = () => <LocalUnconfiguredComponent />;
