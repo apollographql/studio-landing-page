204d30a0903a1996f17a1fb77e500528403951b5
David Glasser <glasser@apollographql.com>
2021-06-02 21:16:12 -0700

    Take apolloStudioEnv into account for redirect (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,6 @@ import ProdConfigured from './content/ProdConfigured';
 import ProdUnconfigured from './content/ProdUnconfigured';
 
 export default () => {
-  // https://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
-  const getCookieValue = (name: string) =>
-    document.cookie.match(`(^|;)\\s*${name}\\s*=\\s*([^;]+)`)?.pop() || '';
-  if (getCookieValue('redirect') === 'true') {
-    window.location.replace(
-      `https://studio.apollographql.com/sandbox?endpoint=${encodeURIComponent(
-        window.location.href,
-      )}`,
-    );
-  }
-
   const {
     graphRef,
     isProd,
@@ -37,6 +26,15 @@ export default () => {
   const baseUrl = `https://studio${
     apolloStudioEnv === 'staging' ? '-staging' : ''
   }.apollographql.com`;
+
+  // https://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
+  const getCookieValue = (name: string) =>
+    document.cookie.match(`(^|;)\\s*${name}\\s*=\\s*([^;]+)`)?.pop() || '';
+  if (getCookieValue('redirect') === 'true') {
+    window.location.replace(
+      `${baseUrl}/sandbox?endpoint=${encodeURIComponent(window.location.href)}`,
+    );
+  }
 
   return (
     <LandingPageBackgroundWrapper>
