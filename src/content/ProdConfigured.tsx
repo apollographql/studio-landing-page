5650a8aff6dec2ac924a1289f87399177c55f79b
Chang Wang <garnwraly@gmail.com>
2021-06-10 11:32:41 -0400

    minor(copy): graph > endpoint
    Signed-off-by: Chang Wang <garnwraly@gmail.com>

--- src/content/ProdConfigured.tsx
+++ src/content/ProdConfigured.tsx
@@ -46,7 +46,7 @@ export default ({
               fontSize: 15,
             }}
           >
-            <p>Send a POST request to query this graph:</p>
+            <p>Send a POST request to query this endpoint:</p>
             <CLIQueryInstructions endpoint={endpoint} />
           </div>
           <p>
@@ -57,7 +57,7 @@ export default ({
               `}
               href={`${baseUrl}/graph/${graphRef}/explorer`}
             >
-              query this graph from Apollo Studio
+              query this endpoint from Apollo Studio
             </a>
           </p>
 
